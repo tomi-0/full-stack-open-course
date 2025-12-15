a5a880f7bd35824771cbfb2caebada54e39ce0bf
tomi-0 <asadetomi@gmail.com>
2025-12-15 23:22:20 +0000

    add header and content component

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,6 +1,28 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,16 +34,10 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1}
+                part2={part2} exercises2={exercises2}
+                part3={part3} exercises3={exercises3}/>
       <p>Number of exercises: {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
