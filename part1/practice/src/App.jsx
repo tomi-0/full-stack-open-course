4657dce2038b7873ff7f53a8fe2df3927ef6e3e0
tomi-0 <asadetomi@gmail.com>
2025-12-17 22:53:21 +0000

    add code to main branch

--- part1/practice/src/App.jsx
+++ part1/practice/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// adding code to main branch
+
 function App() {
   const [count, setCount] = useState(0)
   console.log("Hello again React")
