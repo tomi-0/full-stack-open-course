fe5a708ff115cec8f01266c650ab66af5b6c079d
tomi-0 <asadetomi@gmail.com>
2025-12-18 00:22:18 +0000

    add event handler and pass state

--- part1/practice/src/App2.jsx
+++ part1/practice/src/App2.jsx
@@ -6,11 +6,49 @@ const App = () => {
     // setCounter is assigned a function to modify the state
     const [counter, setCounter] = useState(0) // sets an initial state of 0
 
-    setTimeout( () => setCounter(counter + 1), 1000)
-    //When the state modifying function setCounter is called, React re-renders the component which means that the function body of the component function gets re-executed:
+    //setTimeout( () => setCounter(counter + 1), 1000)
+    //When the state modifying function setCounter is called, 
+    // React re-renders the component which means that the function body of the component function gets re-executed:
 
+    const increment = () => setCounter(counter + 1)
+    
+
+    const reset = () => setCounter(0)
+    
+
+    // An event handler (e.g. onClick) is supposed to be either a function or a function reference
     return (
-        <div>Counter: {counter}</div>
+        <>
+        <Display counter={counter} />
+        
+        {/* We define the event handlers for our buttons where we declare their onClick attributes: */}
+        <Button 
+            label="Increment"
+            func ={increment}
+        />
+
+        {/*  Usually defining event handlers within JSX-templates is not a good ide */}
+        <Button 
+            label='Reset'
+            func={reset}
+        />
+        </>
+    )
+
+}
+
+const Display = (props) => {
+    return (
+        <div>Counter: {props.counter}</div>
+    )
+}
+
+// event handler is passed to the Button component through the func prop
+const Button = (props) => {
+    return (
+        <button onClick={props.func}>
+            {props.label}
+        </button>
     )
 }
 
