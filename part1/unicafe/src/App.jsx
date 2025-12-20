298246e6668b10652bff458c92c771db3302b87d
tomi-0 <asadetomi@gmail.com>
2025-12-20 21:34:17 +0000

    unicafe step 2 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [score, setScore] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+    setScore(score + 1)
+    setTotal(total + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setScore(score + 0)
+    setTotal(total + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+    setScore(score - 1)
+    setTotal(total + 1)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>give feedback</h1>
+
+      <Button text="good" handler={handleGood}/>
+      <Button text="neutral" handler={handleNeutral}/>
+      <Button text="bad" handler={handleBad}/>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>netral {neutral}</p>
+      <p>bad {bad}</p>
+
+      <p>all {total}</p>
+      <p>average {score/total}</p>
+      <p>positive {(good/total) * 100}%</p>
     </>
+  )
+}
+
+const Button = (props) => {
+  const  {text, handler} = props
+  return (
+    <button onClick={handler}>{text}</button>
   )
 }
 
